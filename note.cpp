7084374a5f829cd2a182847b0331b2ecde7ddaf6
Tkorp <15080606710@163.com>
2026-05-11 21:22:26 +0800

    Update note.cpp
    update note.cpp

--- note.cpp
+++ note.cpp
@@ -1,3 +1,25 @@
 #include "note.h"
 
-Note::Note() {}
+Note::Note(qint64 _targetTime, NoteType _type, QWidget* parent)
+    :QLabel(parent),targetTime(_targetTime),type(_type){}
+
+Note::~Note(){
+
+}
+
+qint64 Note::getTargetTime()const{
+    return targetTime;
+}
+
+int Note::getCurrentY()const{
+    return currentY;
+}
+
+NoteType Note::getType()const{
+    return type;
+}
+
+void Note::updateY(int newY){
+    currentY=newY;
+    return;
+}
