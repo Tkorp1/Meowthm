fe5f710e319a9e6c1eed5fc462c871f274153636
intelligentsaltyfish-0818 <intelligentsaltyfish@gmail.com>
2026-05-14 19:35:14 +0800

    新建了mapparser class

--- mapparser.h
+++ mapparser.h
@@ -1,8 +1,18 @@
 #ifndef MAPPARSER_H
 #define MAPPARSER_H
 
+#include "note.h"
+#include "track.h"
+
+class GameScene;
+
+
 class MapParser
 {
+protected:
+    int currentBPM; // 记录曲目bpm
+    GameScene* currentGameScene; // 记录当前游戏界面，方便给新生成的音符 parent
+    Track** ptrTracks; // 指向四个轨道的指针
 public:
     MapParser();
 };
