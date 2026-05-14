9cf7d41939ef4d979cca051a993abfda6fecc876
Tkorp <15080606710@163.com>
2026-05-14 20:16:56 +0800

    test

--- gamescene.cpp
+++ gamescene.cpp
@@ -1,5 +1,16 @@
 #include "gamescene.h"
+#include "mapparser.h"
 
 GameScene::GameScene(QWidget *parent)
     : QWidget{parent}
-{}
+{
+    int __hitLineY = 100;
+    int trackWidth = 200;
+    int xpos = 300;
+    for(int jj = 0; jj < 4;++jj){
+        Track[jj] = new Track(jj, __hitLineY, jj * trackWidth + xpos, this);
+        //下面开始为轨道使用mapparser创建音符
+        MapParser temp=MapParser();
+        temp(&(Track[jj]));
+    }
+}
