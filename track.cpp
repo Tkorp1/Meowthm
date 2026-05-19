186bf39d1e9c8baf4d45a75f791f5e2308d2f885
Tkorp <15080606710@163.com>
2026-05-19 19:34:12 +0800

    1

--- track.cpp
+++ track.cpp
@@ -1,10 +1,24 @@
 #include "track.h"
 #include "note.h"
 
-Track::Track() {}
+Track::Track(int _trackIndex, int _hitLineY, int _xPos, QObject* parent):
+    QObject(parent),trackIndex(_trackIndex),xPos(_xPos),hitLineY(_hitLineY)
+{
+
+}
+
+Track::~Track(){}
+
+void Track::addNotes(const QList<Note*> noteList){
+    noteInTrack=noteList;
+}
+
+void Track::updateTrack(qint64 currentMusicTime, double currentSpeed){
+
+}
 
 void Track::setNoteParent(QWidget* parent){
-    for(Note* note : noteInTrack){
+    for(Note* note : std::as_const(noteInTrack)){
         note->setParent(parent);
     }
 }
