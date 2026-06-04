923af0e132f4819cdb560c33bdc769b2749a1046
Tkorp <15080606710@163.com>
2026-06-05 00:17:11 +0800

    song

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include "mainwindow.h"
 #include "gamescene.h"
-
+#include "resultscene.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
@@ -10,9 +10,11 @@ int main(int argc, char *argv[])
     // MainWindow w;
     // w.show();
 
-    GameScene* testGame = new GameScene("/Users/rose/code/QT/Meowthm/maps/song1"); // 这里写的是绝对地址，到时候记得改
+    GameScene* testGame = new GameScene("/Users/tkorp/Documents/GitHub/Meowthm1/maps/song1"); // 这里写的是绝对地址，到时候记得改
     testGame->show();
-
+    //GameState temp("song");
+    //ResultScene* testresult = new ResultScene(temp);
+    //testresult->show();
     return a.exec();
 
 }
